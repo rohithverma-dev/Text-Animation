3c08391a43c9617559c0c9a6d74844e83695486a
rohit verma <rohithverma.dev@gmail.com>
2024-07-07 15:26:11 +0530

    Text-animation done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1>Skills Text Animation</h1>
+      <div className="outer">
+        <div className="main">
+          <div className="container">
+            <div className="skills">Frontend Developer</div>
+            <div className="skills">Backend Developer</div>
+            <div className="skills">UI/UX Designer</div>
+          </div>
+        </div>   
+      </div>
+    </>
+  )
+}
+
+export default App
